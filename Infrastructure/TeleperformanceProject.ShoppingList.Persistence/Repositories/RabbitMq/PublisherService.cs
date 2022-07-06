ed8433aeb24d853de869da01393083d6f7a4db7e
Mert Bayraktar <83560721+mertbayraktar335@users.noreply.github.com>
2022-07-07 02:46:25 +0300

    Update PublisherService.cs

--- Infrastructure/TeleperformanceProject.ShoppingList.Persistence/Repositories/RabbitMq/PublisherService.cs
+++ Infrastructure/TeleperformanceProject.ShoppingList.Persistence/Repositories/RabbitMq/PublisherService.cs
@@ -19,21 +19,21 @@ namespace TeleperformanceProject.ShoppingList.Persistence.Repositories.RabbitMq
 
         public void Publish(ShopListDto dto, string queueName, string routingKey)
         {
-            //Creating the RabbitMQ connection with Using Because it must close connection when it send the message!
+            
             using var connection = _rabbitMqConnection.GetRabbitMqConnection();
             using var channel = connection.CreateModel();
 
-            //Creating exchanges and queues
+           
             channel.ExchangeDeclare(exchange: "direct.test", type: "direct", durable: false, autoDelete: false);
             channel.QueueDeclare(queue: queueName, durable: false, exclusive: false, autoDelete: true);
 
-            //Binding the exchanges and queues created into eachother with routingkey
+            
             channel.QueueBind(queue: queueName, exchange: "direct.test", routingKey: routingKey);
 
-            //User is an object, for that changing that into Bytes..
+           
             var messageBody = Encoding.UTF8.GetBytes(JsonSerializer.Serialize(dto));
 
-            //At last, we are publishing the message.
+            
             channel.BasicPublish(exchange: "direct.test", routingKey: routingKey, basicProperties: null, body: messageBody);
         }
     }
